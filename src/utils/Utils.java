ece2418c46a2a5d334a2cc06f72811479bb59951
sulin <sulin@MJESUS>
2024-01-19 15:32:28 +0100

    cam

--- src/utils/Utils.java
+++ src/utils/Utils.java
@@ -1,22 +1,29 @@
 package utils;
 
-
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Scanner;
 
 import javax.swing.JOptionPane;
 
 public class Utils {
+	
+	public static void main(String args[]) {
+		int num = Utils.obtenerNumeroAzar(10, 20);
+		System.out.println(num);
+	}
 
 	/**
 	 * Método que obtiene y devuelve un número al azar, entre 0 y 100.
+	 * 
 	 * @return Número generado al azar entre 0 y 100
 	 */
-	public static int obtenerNumeroAzar () {
-		 return (int) Math.round(Math.random() * 100);
+	public static int obtenerNumeroAzar() {
+		return (int) Math.round(Math.random() * 100);
 	}
-	
 
 	/**
 	 * 
@@ -24,11 +31,9 @@ public class Utils {
 	 * @param max Límite superior de generación del número al azar
 	 * @return Número generado al azar entre dos límites.
 	 */
-	public static int obtenerNumeroAzar (int min, int max) {
-		 return (int) Math.round(Math.random() * (max - min)) + min;
+	public static int obtenerNumeroAzar(int min, int max) {
+		return (int) Math.round(Math.random() * (max - min)) + min;
 	}
-	 
- 
 	/**
 	 * Pide un número entero al usuario
 	 * @param mensaje String que va a mostrarse en consola para pedir el número
@@ -54,140 +59,105 @@ public class Utils {
 		return n;
 	}
 	
-	
-	
 	/**
-	 * Obtiene un número entero introducido por el usuario, por el método de InputStreamReader
+	 * Obtiene un número entero introducido por el usuario, por el método de
+	 * InputStreamReader
+	 * 
 	 * @return Número entero introducido por el usuario.
 	 */
-	public static int obtenerEnteroPorInputStreamReader () {
+	public static int obtenerEnteroPorInputStreamReader() {
 		int numero = 0;
 		try {
 			InputStreamReader isr = new InputStreamReader(System.in);
-			BufferedReader br = new BufferedReader (isr);
-			numero = Integer.parseInt (br.readLine());
+			BufferedReader br = new BufferedReader(isr);
+			numero = Integer.parseInt(br.readLine());
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		return numero;
 	}
 
-	
 	/**
 	 * Obtiene un número entero a través de un objeto Scanner
+	 * 
 	 * @return Número entero introducido por el usuario.
 	 */
 	public static int obtenerEnteroPorScanner() {
 		Scanner sc = new Scanner(System.in);
-		
-		while (true) {
-			try {
-				return sc.nextInt();
-			} catch (Exception ex) {
-				System.out.println("Error, introduce un número entero: ");
-				sc.nextLine();
-			}
-		}
-		
+		return sc.nextInt();
 	}
-	
+
 	/**
 	 * Obtiene un número entero a través de un JOptionPane
+	 * 
 	 * @return Número entero introducido a través de un JOptionPane.
 	 */
-	public static int obtenerEnteroPorJOptionPane() {
-		String str = JOptionPane.showInputDialog("Introduce un número");
+	public static int obtenerEnteroPorJOptionPaneConDescripcion(String desc) {
+		String str = JOptionPane.showInputDialog(desc);
 		int numEntero = Integer.parseInt(str);
 		return numEntero;
 	}
 	
-	
-	/**
-	 * 
-	 * @param mensaje
-	 * @return
-	 */
-	public static int obtenerEnteroPorJOptionPane (String mensaje) {
-		String str = JOptionPane.showInputDialog(mensaje);
-		int numEntero = Integer.parseInt(str);
-		return numEntero;
+	public static String obtenerStringPorJOptionPaneConDescripcion(String desc) {
+		String str = JOptionPane.showInputDialog(desc);
+		return str;
 	}
 	
 	
+	public static float obtenerFloatPorJOptionPaneConDescripcion(String desc) {
+		String str = JOptionPane.showInputDialog(desc);
+		float num = Float.parseFloat(str);
+		return num;
+	}
+
+	public static int obtenerEnteroPorJOptionPaneEntreLimitesYDescripcion(int min, int max, String desc) {
+		
+		int numEntero;
+		do {
+			numEntero = Utils.obtenerEnteroPorJOptionPaneConDescripcion(desc);
+		} while (numEntero <= min || numEntero >= max);
+		
+		return numEntero;
+
+	}
+
 	public static int obtenerEnteroConDescripcion(String desc) {
 		System.out.println(desc);
 		int num = obtenerEnteroPorScanner();
 		return num;
 	}
-	
+
 	/**
 	 * 
 	 * @return Número entero introducido por el usuario
 	 */
 	public static int obtenerEntero() {
-		return obtenerEnteroPorJOptionPane();
+		return obtenerEnteroPorScanner();
 	}
 	
-	/**
-	 * 
-	 * @param mensajeAlUsuario
-	 * @param limiteInf
-	 * @param limiteSup
-	 * @return
-	 */
-	public static int obtenerEnteroEntreLimites (String mensajeAlUsuario, int limiteInf,
-			int limiteSup) {
-		int numeroADelvolver = 0;
+	public static Date parseoFecha(String fechaSinParseo) {
 		
-		Scanner sc = new Scanner(System.in);
-		boolean datoCorrecto;
-		do {
-			try {
-				datoCorrecto = true;
-				System.out.println(mensajeAlUsuario);
-				numeroADelvolver = sc.nextInt();
+		SimpleDateFormat formatoFecha = new SimpleDateFormat("yyyy-MM-dd");
 
-				if (numeroADelvolver < limiteInf || numeroADelvolver > limiteSup) {
-					datoCorrecto = false;
-					System.out.println("ERROR, debes introducir un número entre " +
-							limiteInf + " y " + limiteSup);
-				}
-			}
-			catch (Exception ex) {
-				System.out.println("ERROR, no has introducido un dato númerico entre " + 
-						limiteInf + " y " + limiteSup);
-				sc.next();
-				datoCorrecto = false;
-			}
-		} while (!datoCorrecto);
+		String fecha = fechaSinParseo;
 
-		return numeroADelvolver;
-	}
-	
-	/**
-	 * 
-	 * @return
-	 */
-	public static String obtenerCadena () {
-		Scanner sc = new Scanner(System.in);
-		return sc.nextLine();
-	}
-	
-	/**
-	 * 
-	 * @return
-	 */
-	public static String obtenerCadenaConDescripcion (String desc) {
-		Scanner sc = new Scanner(System.in);
-		System.out.println(desc);
-		return sc.nextLine();
-	}
+		Date fechaParseada = null;
 
+		try {
+			fechaParseada = formatoFecha.parse(fecha);
+		} catch (ParseException e) {
+			System.out.println("Error en el parseo de la fecha");
+			e.printStackTrace();
+		}
+		
+		return fechaParseada;
+		
+		
+	}
 
 	public static String obtenerStringPorScanner() {
 		// TODO Auto-generated method stub
 		return null;
 	}
-	
-}
 
+}
--- src/utils/UtilsArrays.java
+++ src/utils/UtilsArrays.java
@@ -3,31 +3,17 @@ package utils;
 
 
 public class UtilsArrays {
-
 	/**
 	 * Imprime el array en consola, de forma que queda en una única línea y con
 	 * un espacio en blanco entre valores
 	 * @param num
 	 */
-	public static void imprimeArray (int a[]) {
-		for (int i = 0; i < a.length; i++) {
-			System.out.print(a[i] + " ");
+	public static void imprimeArray (char[] comb) {
+		for (int i = 0; i < comb.length; i++) {
+			System.out.print(comb[i] + " ");
 		}
 		System.out.println();
 	}
-
-	/**
-	 * Imprime el array en consola, de forma que queda en una única línea y con
-	 * un espacio en blanco entre valores
-	 * @param num
-	 */
-	public static void imprimeArray (char a[]) {
-		for (int i = 0; i < a.length; i++) {
-			System.out.print(a[i] + " ");
-		}
-		System.out.println();
-	}
-
 	/**
 	 * 
 	 * @param a
@@ -40,66 +26,141 @@ public class UtilsArrays {
 		}
 	}
 	
-	
 	/**
 	 * 
 	 * @param array
-	 * @return
 	 */
-	public static int suma (int array[]) {
-		int suma = 0;
+	public static void mostrarArrayConDescripcion (int[] array, String texto) {
+		System.out.println(texto);
 		for (int i = 0; i < array.length; i++) {
-			suma += array[i];
+			System.out.print(array[i] + " ");
 		}
-		return suma;
+		System.out.println();
 	}
 	
-	
-	/**
-	 * 
-	 * @param array
-	 * @return
-	 */
-	public static float promedia (int array[]) {
-		return suma(array) / (float) array.length;
+	public static void mostrarArray (int[] array) {
+		System.out.println("Contenido del array");
+		for (int i = 0; i < array.length; i++) {
+			System.out.print(array[i] + " ");
+		}
+		System.out.println();
 	}
 	
-	
-	/**
-	 * 
-	 * @param array
-	 * @return
-	 */
-	public static int mayor (int array[]) {
-		int mayor = array[0];
-		for (int i = 1; i < array.length; i++) {
-			if (array[i] > mayor) mayor = array[i];
+	public static void mostrarArrayDouble (double[] array) {
+		System.out.println("Contenido del array");
+		for (int i = 0; i < array.length; i++) {
+			System.out.print(array[i] + " ");
 		}
-		return mayor;
+		System.out.println();
 	}
 	
-	/**
-	 * 
-	 * @param array
-	 * @return
-	 */
-	public static int menor (int array[]) {
-		int menor = array[0];
-		for (int i = 1; i < array.length; i++) {
-			if (array[i] < menor) menor = array[i];
+	public static void mostrarArrayChar (char[] array) {
+		System.out.println("Contenido del array");
+		for (int i = 0; i < array.length; i++) {
+			System.out.print(array[i] + " ");
 		}
-		return menor;
+		System.out.println();
+	}
+	
+	public static void mostrarArrayFloat (float[] array) {
+		System.out.println("Contenido del array");
+		for (int i = 0; i < array.length; i++) {
+			System.out.print(array[i] + " ");
+		}
+		System.out.println();
 	}
 
+	/**
+	 * 
+	 * @param longitud
+	 * @param limInf
+	 * @param limSup
+	 * @return
+	 */
+	public static int[] creaArrayNumerosAzar (int longitud, int limInf, int limSup) {
+		int array[] = new int[longitud];
+		for (int i = 0; i < array.length; i++) {
+			array[i] = Utils.obtenerNumeroAzar(limInf, limSup);
+		}
+		return array;
+	}
+
+	/**
+	 * 
+	 * @param longitud
+	 * @param limInf
+	 * @param limSup
+	 * @return
+	 */
+	public static int[] creaArrayNumerosUsuario (int longitud) {
+		int array[] = new int[longitud];
+		for (int i = 0; i < array.length; i++) {
+			array[i] = Utils.obtenerEntero();
+		}
+		return array;
+	}
 	
 	
 	/**
 	 * 
-	 * @param filas
-	 * @param cols
-	 * @param valorInicial
-	 * @return
+	 * @param array
 	 */
+	public static void ordenaArray (int array[]) {
+
+	
+		
+		boolean hayIntercambios;
+		do {
+			hayIntercambios = false;
+			// Empieza el algoritmo
+			for (int i = 0; i < array.length - 1; i++) {
+				if (array[i+1] < array[i]) {
+					// Entonces hago un intercambio
+					int aux = array[i+1];
+					array[i+1] = array[i];
+					array[i] = aux;
+					hayIntercambios = true;
+				}
+			}
+		} while (hayIntercambios);
+
+	}
+	
+	public static void inicializaMatriz (int matriz[][]) {
+		for (int i = 0; i < matriz.length; i++) {
+			for (int j = 0; j < matriz[i].length; j++) {
+				matriz[i][j] = Utils.obtenerNumeroAzar(0, 100);
+			}
+		}
+	}
+	
+	public static void inicializaMatriz1000 (int matriz[][]) {
+		for (int i = 0; i < matriz.length; i++) {
+			for (int j = 0; j < matriz[i].length; j++) {
+				matriz[i][j] = Utils.obtenerNumeroAzar(0, 1000);
+			}
+		}
+	}
+	
+	public static void mostrarMatriz (int matriz[][]) {
+		System.out.println("Contenido de la matriz");
+		for (int i = 0; i < matriz.length; i++) {
+			for (int j = 0; j < matriz[i].length; j++) {
+				System.out.print(matriz[i][j] + "\t");
+			}
+			System.out.println();
+		}
+	}
+	
+	public static void mostrarMatrizChar (char matriz[][]) {
+		System.out.println("Contenido de la matriz");
+		for (int i = 0; i < matriz.length; i++) {
+			for (int j = 0; j < matriz[i].length; j++) {
+				System.out.print(matriz[i][j] + "\t");
+			}
+			System.out.println();
+		}
+	}
 	public static int[][] creaEInicializaMatriz (int filas, int cols, int valorInicial) {
 		int m[][] = new int[filas][cols];
 		
@@ -111,83 +172,34 @@ public class UtilsArrays {
 		
 		return m;
 	}
-	
-	
-	/**
-	 * 
-	 * @param filas
-	 * @param cols
-	 * @param minimo
-	 * @param maximo
-	 * @return
-	 */
-	public static int[][] creaEInicializaMatrizAlAzar (
-			int filas, int cols, int minimo, int maximo) {
+	public static void imprimeMatriz(int[][] m1) {
+		// TODO Auto-generated method stub
 		
-		int m[][] = new int[filas][cols];
-		
-		for (int i = 0; i < m.length; i++) {
-			for (int j = 0; j < m[i].length; j++) {
-				m[i][j] = Utils.obtenerNumeroAzar(minimo, maximo);
-			}
-		}
-		
-		return m;
 	}
-	
-	
-	/**
-	 * 
-	 * @param m
-	 */
-	public static void imprimeMatriz (int m[][]) {
-		for (int i = 0; i < m.length; i++) {
-			for (int j = 0; j < m[i].length; j++) {
-				System.out.print(m[i][j] + "\t");
-			}
-			System.out.println();
-		}
+	public static void imprimeArray(int[] m) {
+		// TODO Auto-generated method stub
+		
+	}
+	public static int[][] creaEInicializaMatrizAlAzar(int i, int j, int k, int l) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	public static String suma(int[] array) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	public static String promedia(int[] array) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	public static String menor(int[] array) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	public static String mayor(int[] array) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
-	
-	public static void ordenaArray (int a[], boolean asc) {
-		boolean hayIntercambios;
-		do {
-			hayIntercambios = false;
-			for (int i = 0; i < (a.length - 1); i++) {
-				if ((asc == true && a[i] > a[i + 1]) 
-						|| (asc == false && a[i] < a[i + 1])) {
-					int aux = a[i];
-					a[i] = a[i + 1];
-					a[i + 1] = aux;
-					hayIntercambios = true;
-				}
-			}
-		} while (hayIntercambios == true);
 
-	}
-
-	public static void ordenaArray(int[] a) {
-		ordenaArray(a, true);		
-	}
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
